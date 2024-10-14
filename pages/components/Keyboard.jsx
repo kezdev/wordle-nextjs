b4f8c9420898b640b8fe236c71785eab3a7caf19
Kez <58428891+kezdev@users.noreply.github.com>
2024-10-14 19:29:00 +0100

    keyboard component

--- pages/components/Keyboard.jsx
+++ pages/components/Keyboard.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-const Keyboard = () => {
+const Keyboard = ({ onKeyPress }) => {
+    const keys = [
+        ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+        ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
+    ];
+
     return (
-        <h1>keyboard component!</h1>
-    )
-}
+        <div className="keyboard">
+            {keys.map((row, i) => (
+                <div className="keyboard-row" key={i}>
+                    {row.map((key) => (
+                        <button key={key} onClick={() => onKeyPress(key)}>
+                            {key}
+                        </button>
+                    ))}
+                </div>
+            ))}
+            <div className="keyboard-row">
+                <button onClick={() => onKeyPress('ENTER')}>ENTER</button>
+                <button onClick={() => onKeyPress('DELETE')}>DELETE</button>
+            </div>
+        </div>
+    );
+};
 
 export default Keyboard;
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,16 @@ import Grid from "@/pages/components/Grid";
 import Keyboard from "@/pages/components/Keyboard";
 
 export default function Home() {
-  return ( <div className="game-container">
-       <h1>Wordle!</h1>
-       <Grid />
-       <Keyboard />
-     </div>
-  );
+
+    const keyPressed = (key) => {
+        console.log(key);
+    }
+
+    return (
+        <div className="game-container">
+            <h1>Wordle!</h1>
+            <Grid />
+            <Keyboard onKeyPress={keyPressed}/>
+        </div>
+    );
 }
